5b9b0d439591343168e37a558409e9d82f6c1b31
csimoes <52419438+c-simoes@users.noreply.github.com>
2020-02-11 14:58:33 +0000

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors({ optionSuccessStatus: 200 }));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 // app.get("/api/hello", function(req, res) {
@@ -36,6 +36,6 @@ app.get("/api/timestamp", (req, res) => {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT, function() {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
